8996ec49fcac952c1acd1b588f06b8b7973df8d1
tbgitoo <thomas.braschler@unige.ch>
2019-11-25 22:47:31 +0100

    Added ImageTools class

--- calciumImaging/src/FindPeaks/accessory/classes/FindPeaksTools.java
+++ calciumImaging/src/FindPeaks/accessory/classes/FindPeaksTools.java
@@ -1046,25 +1046,6 @@ public class FindPeaksTools {
 
 
 
-	// Helper function to get a new stack of the same dimensions as 
-	// given stack for creating masks
-	public static ImageStack getEmptyByteStack(ImageStack source)
-	{
-		ImageStack theStack = new ImageStack(source.getWidth(), 
-				source.getHeight(), source.getSize());
-
-
-
-		for(int ind=1; ind<=theStack.getSize(); ind++)
-		{
-			theStack.setProcessor(
-					new ByteProcessor(theStack.getWidth(), 
-							theStack.getHeight()), ind);
-		}
-
-		return(theStack);
-
-
-	}
+	
 
 }
